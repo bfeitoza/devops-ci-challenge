02aa3468ddf9dc0bc798e4a8c16cb6d260541985
bfeitoza <brunor.feitoza@gmail.com>
2022-06-05 23:57:10 -0300

    My infra to static web site

---
+++ infra/envs/devel/variables.tf
@@ -0,0 +1,18 @@
+variable aws_region {
+  type        = string
+  description = "The name of the region"
+}
+
+variable project_name {
+  type        = string
+  description = "The name of the project"
+}
+
+variable env {
+  type        = string
+  description = "Application environment name"
+}
+
+variable common_tags {
+  description = "Common tags to out components"
+}
